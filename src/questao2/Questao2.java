fb2a0c406dd32e64f5b76e8f71e875a42247530e
Rodrigo Gregori <rodrigo.gregori@gmail.com>
2021-04-01 20:49:44 -0300

    Corrigir testes

--- src/questao2/Questao2.java
+++ src/questao2/Questao2.java
@@ -3,15 +3,16 @@ package questao2;
 public class Questao2 {
     public static void main(String[] args) {
         Complex c = new Complex();
-        if (!c.toString().equals("(1, 1)")) throw new AssertionError("Construtor padrão ou método toString incorreto. Deveria mostrar (1, 1)");
+        if (!c.toString().equals("(1.0, 1.0)")) throw new AssertionError("Construtor padrão ou método toString incorreto. Deveria mostrar (1, 1)");
         Complex c1 = new Complex(1, 1);
-        if (!c.toString().equals("(1, 1)")) throw new AssertionError("Construtor ou método toString incorreto. Deveria mostrar (1, 1)");
+        if (!c.toString().equals("(1.0, 1.0)")) throw new AssertionError("Construtor ou método toString incorreto. Deveria mostrar (1, 1)");
         c = new Complex(1, 2);
         c1 = new Complex(2, -4);
-        c.sum(c1);
-        if (!c.toString().equals("(3, -2)")) throw new AssertionError("Soma de complexos incorreta. Deveria mostrar (3, -2)");
+        c.sum(c1); // this.real = this.real + c1.real
+                    // this.imaginary = this.imaginary + c1.imaginary
+        if (!c.toString().equals("(3.0, -2.0)")) throw new AssertionError("Soma de complexos incorreta. Deveria mostrar (3, -2)");
         c = new Complex(1, 2);
         c.subtract(c1);
-        if (!c.toString().equals("(-1, 6)")) throw new AssertionError("Soma de complexos incorreta. Deveria mostrar (-1, 6)");
+        if (!c.toString().equals("(-1.0, 6.0)")) throw new AssertionError("Soma de complexos incorreta. Deveria mostrar (-1, 6)");
     }
 }
--- src/questao3/Questao3.java
+++ src/questao3/Questao3.java
@@ -4,12 +4,12 @@ public class Questao3 {
     public static void main(String[] args) {
         Ponto p1 = new Ponto(3, 4);
         if (p1.getX() != 3 || p1.getY() != 4) throw new AssertionError("O valor de x e/ou y estão incorretamente armazenados");
-        double d = p1.distance(5, 6);
+        double d = p1.distancia(5, 6);
         if (Double.compare(d, Math.sqrt(8)) != 0) throw new AssertionError("A distância entre os pontos foi incorretamente calculada");
         Ponto p2 = new Ponto(5, 6);
-        d = p1.distance(p2);
+        d = p1.distancia(p2);
         if (Double.compare(d, Math.sqrt(8)) != 0) throw new AssertionError("A distância entre os pontos foi incorretamente calculada");
-        d = p1.distance();
+        d = p1.distancia();
         if (Double.compare(d, 5) != 0) throw new AssertionError("A distancia entre o ponto e (0, 0) foi incorretamente calculada");
     }
 }
